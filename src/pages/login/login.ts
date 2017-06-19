c829d51f1b708123c6c599a86217607bf484426f
alexnu <alexnu@projectbeagle.com>
2017-06-19 13:09:14 +0300

    Enabled google and facebook providers

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -31,8 +31,8 @@ export class LoginPage {
       credentialHelper: firebaseui.auth.CredentialHelper.NONE,
       signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       // firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
